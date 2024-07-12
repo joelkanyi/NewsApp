cf6eeca3b0c0318f3b2ee8ef7fc98d03660dbdcf
Joel Kanyi <joelkanyi98@gmail.com>
2024-07-12 14:44:59 +0300

    display favorite news

--- presentation/src/main/java/io/github/joelkanyi/presentation/favorites/FavoritesScreen.kt
+++ presentation/src/main/java/io/github/joelkanyi/presentation/favorites/FavoritesScreen.kt
@@ -1,30 +1,52 @@
 package io.github.joelkanyi.presentation.favorites
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import io.github.joelkanyi.designsystem.components.EmptyStateComponent
 import io.github.joelkanyi.designsystem.theme.NewsAppTheme
+import io.github.joelkanyi.presentation.components.NewsItem
+import io.github.joelkanyi.presentation.model.NewsUiModel
+import io.github.joelkanyi.presentation.navigation.Destinations
 
 @Composable
 fun FavoritesScreen(
-    navController: NavController
+    navController: NavController,
+    viewModel: FavoritesViewModel = hiltViewModel()
 ) {
-    FavoritesScreenContent()
+    val favorites by viewModel.favorites.collectAsState(initial = emptyList())
+    FavoritesScreenContent(
+        favorites = favorites,
+        onNewsClick = { news ->
+            navController.navigate(Destinations.NewsDetails(news))
+        }
+    )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun FavoritesScreenContent() {
+fun FavoritesScreenContent(
+    favorites: List<NewsUiModel>,
+    onNewsClick: (NewsUiModel) -> Unit,
+) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -33,18 +55,35 @@ fun FavoritesScreenContent() {
                         text = "Favorites",
                         style = MaterialTheme.typography.titleMedium
                     )
-                })
+                }
+            )
         }
     ) {
         Box(
             modifier = Modifier
                 .padding(it)
                 .fillMaxSize(),
-            contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = "Favorites",
-            )
+            if (favorites.isEmpty()) {
+                EmptyStateComponent(
+                    modifier = Modifier.align(Alignment.Center),
+                    message = "No favorites yet"
+                )
+            } else {
+                LazyColumn(
+                    contentPadding = PaddingValues(
+                        horizontal = 16.dp,
+                    ),
+                    verticalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    items(favorites) { news ->
+                        NewsItem(
+                            news = news,
+                            onClick = { onNewsClick(news) }
+                        )
+                    }
+                }
+            }
         }
     }
 }
@@ -53,6 +92,9 @@ fun FavoritesScreenContent() {
 @Composable
 private fun FavoritesScreenPreview() {
     NewsAppTheme {
-        FavoritesScreenContent()
+        FavoritesScreenContent(
+            favorites = emptyList(),
+            onNewsClick = {}
+        )
     }
 }
---
+++ presentation/src/main/java/io/github/joelkanyi/presentation/favorites/FavoritesViewModel.kt
@@ -0,0 +1,26 @@
+package io.github.joelkanyi.presentation.favorites
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.github.joelkanyi.domain.usecase.favorite.GetFavoritesUseCase
+import io.github.joelkanyi.presentation.model.NewsUiModel.Companion.toUiModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoritesViewModel @Inject constructor(
+    getFavoritesUseCase: GetFavoritesUseCase
+) : ViewModel() {
+    val favorites = getFavoritesUseCase()
+        .map {
+            it.map { news -> news.toUiModel() }
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = emptyList()
+        )
+}
