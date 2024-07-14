8afc55c430687c217268e88e060c15de5033abc7
Joel Kanyi <joelkanyi98@gmail.com>
2024-07-14 08:03:20 +0300

    add temporary color

--- designsystem/src/main/java/io/github/joelkanyi/designsystem/theme/Theme.kt
+++ designsystem/src/main/java/io/github/joelkanyi/designsystem/theme/Theme.kt
@@ -9,37 +9,31 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme =
     darkColorScheme(
-        primary = Purple80,
-        secondary = PurpleGrey80,
-        tertiary = Pink80
+        primary = Color.White,
+        secondary = PrimaryColor,
+        tertiary = PrimaryColor
     )
 
 private val LightColorScheme =
     lightColorScheme(
-        primary = Purple40,
-        secondary = PurpleGrey40,
-        tertiary = Pink40
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-     */
+        primary = PrimaryColor,
+        secondary = PrimaryColor,
+        tertiary = PrimaryColor
     )
 
+
+@Suppress("DEPRECATION")
 @Composable
 fun NewsAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme =
