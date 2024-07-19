8fd1701fb7a3827f9ad6bddd08ba170d9f732370
Joel Kanyi <joelkanyi98@gmail.com>
2024-07-19 08:01:31 +0300

    edit image placeholder

--- presentation/src/main/java/io/github/joelkanyi/presentation/components/NewsImage.kt
+++ presentation/src/main/java/io/github/joelkanyi/presentation/components/NewsImage.kt
@@ -1,16 +1,18 @@
 package io.github.joelkanyi.presentation.components
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Image
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.SubcomposeAsyncImage
@@ -52,13 +54,13 @@ fun NewsImage(
                     .fillMaxSize(),
                 contentAlignment = Alignment.Center
             ) {
-                Image(
-                    painter =
-                    painterResource(
-                        id = io.github.joelkanyi.designsystem.R.drawable.image_placeholder
-                    ),
+                Icon(
+                    modifier = Modifier.size(48.dp),
+                    imageVector = Icons.Outlined.Image,
                     contentDescription = stringResource(R.string.error_loading_image),
-                    modifier = Modifier.size(48.dp)
+                    tint = MaterialTheme.colorScheme.primary.copy(
+                        alpha = .5f
+                    )
                 )
             }
         }
